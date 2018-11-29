490de4bd4ddab288c03864c7536bd50ec13c8727
Pranoy <pranoy.mbstu12001@gmail.com>
2018-11-30 04:05:29 +0600

    update file

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const wincombos = [
 	[2,5,8],
 	[0,4,8],
 	[6,4,2]
-];
+]
 const cells = document.querySelectorAll('.cell');
 
 startGame();
@@ -26,9 +26,65 @@ function startGame() {
 }
 
 function turnClick(square) {
-	turn(square.target.id, huPlayer);
+	if(typeof origBoard[square.target.id] == 'number'){
+		turn(square.target.id, huPlayer);
+		if(!checkTie()) turn(bestSpot(), aiPlayer);
+	}
 }
 
 function turn(squareId, player){
+	origBoard[squareId] = player;
 	document.getElementById(squareId).innerText = player;
+	let gameWon = checkWon(origBoard, player);
+	if(gameWon) gameOver(gameWon);
+}
+
+function checkWon(board, player){
+	let plays = board.reduce((a, e, i) =>
+		(e === player) ? a.concat(i) : a, []);
+	let gameWon = null;
+	for(let [index, win] of wincombos.entries()){
+		if(win.every(elem => plays.indexOf(elem) > -1)){
+			gameWon = {index: index, player: player};
+			break;
+		}
+	}
+	return gameWon;
+}
+
+function gameOver(gameWon){
+	for(let index of wincombos[gameWon.index]){
+		document.getElementById(index).style.backgroundColor =
+		gameWon.player == huPlayer ? "blue" : "red";
+	}
+
+	for(var i = 0; i < cells.length; i++){
+		cells[i].removeEventListener('click', turnClick, false);
+	}
+	declareWinner(gameWon.player == huPlayer ? "You Win." : "You lose!");
+}
+
+function declareWinner(who){
+	document.querySelector(".endGame").style.display = "block";
+	document.querySelector(".endGame .text").innerText = who;
+}
+
+function emptySquare(){
+	return origBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+	return emptySquare()[0];
+}
+
+function checkTie(){
+	if(emptySquare().length == 0){
+		for(var i = 0; i <cells.length; i++){
+			cells[i].style.backgroundColor = 'green';
+			cells[i].removeEventListener('click', turnClick, false);
+		}
+		declareWinner("Tie Game!");
+		return true;
+	}
+	return false;
 }
